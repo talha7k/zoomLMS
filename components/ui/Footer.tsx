61489436f476f16516a11b80cee07d35dbb7f55c
talha7k <talha7k@gmail.com>
2025-02-24 03:21:47 +0500

    'footer-'

---
+++ components/ui/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="w-full text-center py-4 text-gray-600">
+        &copy; 2025 Talha Khan
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
